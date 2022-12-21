599a3db62354c5fb8da62fc55d7934d9aa9c054e
Arunprasath Shankar <arunprsh@gmail.com>
2022-12-21 23:41:20 +0000

    wip

---
+++ evaluation.py
@@ -0,0 +1,27 @@
+from sklearn.metrics import f1_score
+import pandas as pd
+import pathlib
+import json
+import os
+
+
+if __name__ == '__main__':
+    y_pred_path = "/opt/ml/processing/input/predictions/x_test.csv.out"
+    y_pred = pd.read_csv(y_pred_path, header=None)
+    y_true_path = "/opt/ml/processing/input/true_labels/y_test.csv"
+    y_true = pd.read_csv(y_true_path, header=None)
+    
+    report_dict = {
+        "classification_metrics": {
+            "weighted_f1": {
+                "value": f1_score(y_true, y_pred, average="weighted"),
+                "standard_deviation": "NaN",
+            },
+        },
+    }
+    
+    output_dir = "/opt/ml/processing/evaluation"
+    pathlib.Path(output_dir).mkdir(parents=True, exist_ok=True)
+    evaluation_path = os.path.join(output_dir, "evaluation_metrics.json")
+    with open(evaluation_path, "w") as f:
+        f.write(json.dumps(report_dict))
