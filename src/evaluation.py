5c563eea483ed8e2560678c30b35f823e106e224
arunprsh <arunprsh@amazon.com>
2022-12-23 23:07:29 +0000

    wip

--- src/evaluation.py
+++ src/evaluation.py
@@ -6,7 +6,13 @@ import json
 import os
 
 
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+logger.addHandler(logging.StreamHandler())
+
+
 if __name__ == '__main__':
+    logger.info('Starting evaluation step')
     y_pred_path = '/opt/ml/processing/input/predictions/holdout.csv.out'
     y_pred = pd.read_csv(y_pred_path, header=None)
     y_true_path = '/opt/ml/processing/input/true_labels/true_labels.csv'
